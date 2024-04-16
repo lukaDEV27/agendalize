47ed179b63f7af38f7e723d5029d7ef829fe3d73
Lucas Luiz da Silva <lucas.oficial2727@gmail.com>
2024-04-15 22:56:13 -0300

    Initial commit

---
+++ src/main/java/br/com/agendalize/AgendalizeApplication.java
@@ -0,0 +1,13 @@
+package br.com.agendalize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendalizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendalizeApplication.class, args);
+	}
+
+}
